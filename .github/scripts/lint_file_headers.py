8cb3a588e85a71d236bc1894078c9fe5c5db476e
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2023-01-13 09:34:18 +0100

    Add Header Linting

--- .github/scripts/lint_file_headers.py
+++ .github/scripts/lint_file_headers.py
@@ -16,15 +16,23 @@ for parent,_,files in os.walk("."):
             continue
         source_files.append(os.path.join(parent, file))
 
-
+error = False
 for file in source_files:
     with open(file, encoding="utf-8") as fp:
         content = fp.read()
-    if content.contains(header):
+    if header in content:
         continue
     if format:
         with open(file, "w", encoding="utf-8") as fp:
             fp.write(header)
             fp.write("\n")
             fp.write(content)
-    print(file, "does not start with header")
+    else:
+        error = True
+        print(file, "does not start with header")
+
+
+if error:
+    sys.exit(1)
+else:
+    print("All files are up to date")
