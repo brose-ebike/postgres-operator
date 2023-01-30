86a200560f9dde9f3bfb116ab2e411c33d115a59
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2023-01-30 11:36:04 +0100

    BRS-4401 Fix Tests

--- api/v1/pguser_types_test.go
+++ api/v1/pguser_types_test.go
@@ -15,3 +15,88 @@ limitations under the License.
 */
 
 package v1
+
+import (
+	"encoding/json"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("PgUserDatabase", func() {
+
+	It("gets serialized with owner is nil", func() {
+		// given:
+		instanceSpec := PgUserDatabase{
+			Name:       "mydb",
+			Owner:      nil,
+			Privileges: []string{"CONNECT"},
+		}
+		// when:
+		data, err := json.Marshal(instanceSpec)
+		textual := string(data)
+
+		// then:
+		Expect(err).To(BeNil())
+		Expect(textual).To(Equal("{\"name\":\"mydb\",\"privileges\":[\"CONNECT\"]}"))
+	})
+
+	It("gets serialized with owner is true", func() {
+		// given:
+		cTrue := true
+		instanceSpec := PgUserDatabase{
+			Name:       "mydb",
+			Owner:      &cTrue,
+			Privileges: []string{"CONNECT"},
+		}
+		// when:
+		data, err := json.Marshal(instanceSpec)
+		textual := string(data)
+
+		// then:
+		Expect(err).To(BeNil())
+		Expect(textual).To(Equal("{\"name\":\"mydb\",\"owner\":true,\"privileges\":[\"CONNECT\"]}"))
+	})
+
+	It("gets serialized with owner is false", func() {
+		// given:
+		cFalse := false
+		instanceSpec := PgUserDatabase{
+			Name:       "mydb",
+			Owner:      &cFalse,
+			Privileges: []string{"CONNECT"},
+		}
+		// when:
+		data, err := json.Marshal(instanceSpec)
+		textual := string(data)
+
+		// then:
+		Expect(err).To(BeNil())
+		Expect(textual).To(Equal("{\"name\":\"mydb\",\"owner\":false,\"privileges\":[\"CONNECT\"]}"))
+	})
+
+	It("IsOwner returns correct value", func() {
+		// given:
+		cFalse := false
+		cTrue := true
+		instanceSpec0 := PgUserDatabase{
+			Name:       "mydb",
+			Owner:      nil,
+			Privileges: []string{"CONNECT"},
+		}
+		instanceSpec1 := PgUserDatabase{
+			Name:       "mydb",
+			Owner:      &cFalse,
+			Privileges: []string{"CONNECT"},
+		}
+		instanceSpec2 := PgUserDatabase{
+			Name:       "mydb",
+			Owner:      &cTrue,
+			Privileges: []string{"CONNECT"},
+		}
+		// then:
+		Expect(instanceSpec0.IsOwner()).To(BeFalse())
+		Expect(instanceSpec1.IsOwner()).To(BeFalse())
+		Expect(instanceSpec2.IsOwner()).To(BeTrue())
+	})
+})
